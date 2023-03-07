c77f2f1d8e160c177e9c0af493af18ebc5cc98f0
Janitha Tharaka <81977457+Janitha-Tharaka@users.noreply.github.com>
2023-03-07 22:44:23 +0530

    Update settings.py

--- chilldressing/settings.py
+++ chilldressing/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd3@iap!+7rfu69@+&$^vqw6oa(hmq7wn(-pju2zt-!)2@)q#rn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
